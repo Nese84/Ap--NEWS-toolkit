ea0fdb22a54b9bc87ed55527244eea90a2de5aba
Nese84 <nbserbest84@gmail.com>
2023-03-23 17:06:23 +0300

    extrareducer

--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -1,17 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios"
 
 const initialState = {
+    news: [],
 
 }
+export const getNews = createAsyncThunk(
+    "getNews", 
+    async (thunkAPI, { rejectWithValue }) => {
+        const API_KEY = "febd459aa6484b9d8384f045fdc37056"
+        const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 
+        try {
+            const { data } = await axios(url)
+            console.log(data)
+            return data.articles
+          } catch (error) {
+            console.log(error)
+            return rejectWithValue("Something went wrong")
+          }
+
+    }
+    )
+    
 const newsSlice = createSlice({
-    name: "news",
-    initialState,
-    reducers: {
-      clearNews: (state) => {
-        state.news = []
-      },
+  name: "news",
+  initialState,
+  reducers: {
+    clearNews: (state) => {
+      state.news = []
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNews.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(getNews.fulfilled, (state, action) => {
+        state.news = action.payload
+        state.loading = false
+      })
+      .addCase(getNews.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload
+      })
+  },
+})
+
 
     export const { clearNews } = newsSlice.actions
 
